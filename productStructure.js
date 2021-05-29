719db7ae6502adfa748bd2a8920d8368b59ab414
Wayne Rocha <dev.waynerocha@gmail.com>
2021-05-29 01:15:28 -0300

    Initial commit

---
+++ productStructure.js
@@ -0,0 +1,27 @@
+const products = [
+  "preto-PP", 
+  "preto-M", 
+  "preto-G", 
+  "preto-GG", 
+  "preto-GG", 
+  "branco-PP", 
+  "branco-G", 
+  "vermelho-M", 
+  "azul-XG", 
+  "azul-XG", 
+  "azul-XG", 
+  "azul-P"
+]
+
+module.exports = () => {
+  const productsObj = {};
+  products.forEach((product) => {
+    const [productColor, productSize] = product.split('-');
+    if (productColor in productsObj === false)
+      productsObj[productColor] = {};
+    if (productSize in productsObj[productColor] === false)
+      productsObj[productColor][productSize] = 0;
+    
+    productsObj[productColor][productSize] += 1;
+  });
+}
